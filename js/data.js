ed6c915767fd65fac9cfceedb4a93cdeba525ff6
gameboyJ3 <jatin.collabera1411@gmail.com>
2022-04-11 21:27:04 +0530

    Initial Commit
    V1

---
+++ js/data.js
@@ -0,0 +1,27 @@
+var dataJson = {
+    "scenes": [
+		{
+            "sceneName": "1",
+            "image": "1.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "13",
+                    "infoPointsCoordinates": [-2979.76, -1639.57, -3652.77],
+                    "infoHoverText": "13",
+                }, {
+                    "infoPointsName": "69",
+                    "infoPointsCoordinates": [2279.56, -4444.91, -161.72],
+                    "infoHoverText": "69",
+                }
+            ]
+        },
+		{
+            "sceneName": "13",
+            "image": "13.jpg",
+            "infoPoints": [{
+                    "infoPointsName": "1",
+                    "infoPointsCoordinates": [2997.91, -358.26, -3979.23],
+                    "infoHoverText": "1",
+                }]
+        }
+    ]
+};
